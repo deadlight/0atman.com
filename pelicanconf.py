bfc3a52a8861ed6e28841286262bbf09b6aeac22
Tristram Oaten <tristram@oaten.name>
2015-01-10 01:13:48 +0000

    updates

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 AUTHOR = u'0atman'
 AUTHORS = AUTHOR
 SITENAME = u'Articles'
-SITEURL = 'http://articles.0atman.com'
+__SITEURL = 'http://articles.0atman.com'
 
 PATH = 'content'
 
@@ -20,7 +20,7 @@ EXTRA_PATH_METADATA = {
     'extra/CNAME': {'path': 'CNAME'}
 }
 
-THEME = "/home/oatman/pelican-themes/nikhil-theme"
+THEME = "/home/oatman/pelican-themes/pelican-sober"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -33,12 +33,13 @@ AUTHOR_FEED_RSS = None
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+)
 
+PELICAN_SIMPLEGREY_TWITTER_CARD_ACCOUNT = '__fle__'
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
+SOCIAL = (
+        ('Twitter', 'http://twitter.com/0atman'),
+)
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
