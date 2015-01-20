6e8fd1efeedb1f3ea17dbe36ffb2ab545d052cfa
Tristram Oaten <tristram@oaten.name>
2015-01-20 11:15:52 +0000

    path fix

--- pelicanconf.py
+++ pelicanconf.py
@@ -74,8 +74,8 @@ SHOW_ARTICLE_AUTHOR = True
 # DISPLAY_ARTICLE_INFO_ON_INDEX=True
 
 ABOUT_ME = "Gentleman Scientist and musician"
-AVATAR = "images/hex-moon-black.png"
-BANNER = "images/composer.png"
+AVATAR = "/images/hex-moon-black.png"
+BANNER = "/images/composer.png"
 BANNER_SUBTITLE = 'My bow is all strings'
 BANNER_ALL_PAGES = False
 
